ab39f3f02806550a82f46e57b2af51d620b42045
Wojciech Skirło <w.skirlo@ideo.pl>
2024-10-25 17:51:17 +0200

    feat: add viewport object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
 import { ReactNode } from 'react';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import '@/src/styles/styles.scss';
 import styles from './layout.module.scss';
 import HolyLoader from 'holy-loader';
-import SystemBanner from '@/src/components/System/Banner';
+// import SystemBanner from '@/src/components/System/Banner';
 import SystemFooter from '@/src/components/System/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Festivente | WMS'
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false
 };
 
 export default function RootLayout({
