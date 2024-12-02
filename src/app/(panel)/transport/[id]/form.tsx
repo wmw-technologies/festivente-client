e817c24c349dc8fd3d750d0ceb3b503b88db8c9b
salekmichal1 <michal.salek@microsoft.wsei.edu.pl>
2024-12-02 09:08:03 +0100

    feat(transport): update vehicle field to accept multiple selections

--- src/app/(panel)/transport/[id]/form.tsx
+++ src/app/(panel)/transport/[id]/form.tsx
@@ -18,7 +18,7 @@ import UIDatepicker from '@/src/components/UI/Datepicker';
 import UITextarea from '@/src/components/UI/Textarea';
 
 const schema = z.object({
-  vehicle: z.string(),
+  vehicle: z.array(z.string()).min(1),
   driver: z.string(),
   event: z.string(),
   departureTime: z.date(),
@@ -42,7 +42,7 @@ type FormProps = {
 export default function Form({ id, isEdit, data, vehicles, employees, events }: FormProps) {
   const router = useRouter();
   const title = isEdit
-    ? `Formularz edycji transportu: ${data?.departureLocation} - ${data?.destinationLocation} | ${data?.vehicleDetails.registrationNumber}`
+    ? `Formularz edycji transportu: ${data?.departureLocation} - ${data?.destinationLocation} | ${data?.vehicleDetails.map((v) => v.registrationNumber).join(', ')}`
     : 'Formularz dodawania transportu';
 
   const {
@@ -79,7 +79,10 @@ export default function Form({ id, isEdit, data, vehicles, employees, events }: 
 
   function init() {
     if (!data) return;
-
+    setValue(
+      'vehicle',
+      data.vehicleDetails.map((v) => v._id)
+    );
     setValue('driver', data.driver._id);
     setValue('event', data.event._id);
     setValue('departureTime', new Date(data.departureTime));
